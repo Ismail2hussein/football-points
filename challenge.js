627c1e8c40ca93f5e5e6c36ef66e85e3b39f02de
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-05-22 16:54:14 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+let defaultWin = 3,
+    defaultDraw = 1,
+    defaultLoss = 0;
+
+function footballPoints(wins, draws, losses) {
+    return defaultWin * wins + defaultDraw * draws + defaultLoss * losses;
+}
+
+console.log(footballPoints(3, 4, 2));
+console.log(footballPoints(5, 0, 2)); 
+console.log(footballPoints(0, 0, 1));
